cb12be18b4cd38b01be06251b0ea9cf2da15b3f8
TSN Student <student@ulem.org>
2018-12-10 12:01:35 -0500

    ok

---
+++ src/app/Services/user.service.client.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/user.model.client';
+import {Http, Response} from "@angular/http";
+import {map} from "rxjs/operators";
+import {environment} from "../../environments/environment";
+
+// injecting service into module
+
+@Injectable()
+
+export class UserService {
+
+  constructor(private http: Http) {}
+
+  baseUrl = environment.baseUrl;
+
+  createUser(user: User) {
+    const url = this.baseUrl + "/api/user";
+    return this.http.post(url, user).pipe(map((res: Response) => {
+      return res.json();
+      })
+    );
+    
+  }
+   
+
+  findUserById(userId: string) {
+    const url = this.baseUrl + "/api/user/" +
+    userId;
+    return this.http.get(url).pipe(map((res: Response) => {
+      return res.json();
+      })
+
+    );
+  }
+       
+  findUserByUsername(username: string) {
+    const url = this.baseUrl + "/api/user?username=" + username;
+    return this.http.get(url).pipe(map((res: Response) => {
+      return res.json();
+      })
+
+    );
+
+  }
+       
+ 
+  findUserByCredentials(username: string, password: string) {
+    const url = this.baseUrl + "/api/user?username=" + username + "&password=" + password;
+    return this.http.get(url).pipe(map((res: Response) => {
+      return res.json();
+      })
+
+    );
+
+  }  
+
+
+   updateUser(user: User ) {
+    const url = this.baseUrl + "/api/user"
+    return this.http.put(url, user).pipe(map((res: Response) => {
+      return res.json();
+      })
+
+    );
+   } 
+
+}
---
+++ src/app/components/user/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { UserService } from "../../../services/user.service.client"
+import { Router } from "@angular/router";
+import { User } from "../../../models/user.model.client"
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  username: string;
+  password: string;
+  errorFlag: boolean;
+ 
+  
+  constructor(private userService: UserService, private router: Router) {}
+
+  ngOnInit() {}
+ 
+login() {
+  this.userService.findUserByCredentials(this.username, this.password).subscribe((user: User) => {
+    this.router.navigate(["user", user._id]);
+  },
+    (error: any) => {
+      this.errorFlag = true;
+      }
+    );
+  }
+}
